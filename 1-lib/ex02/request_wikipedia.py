3a66ea3ce3ddea0d9227ef1127a757a6ef55a935
Benjamin Perez <bperez@z3r4p4.42lyon.fr>
2023-12-09 14:16:16 +0100

    /home/bperez/Documents/piscine-django/1-lib/ex02 02 + 0-oob 05 lburnet's tests

--- 1-lib/ex02/request_wikipedia.py
+++ 1-lib/ex02/request_wikipedia.py
@@ -3,14 +3,13 @@ import requests, json, dewiki, sys
 def request_wikipedia(title):
     api = "https://en.wikipedia.org/w/api.php"
     search = "?action=opensearch&search={title}"
-    parse = "?action=parse&page={page}&format=json"
+    parse = "?action=parse&page={page}&prop=wikitext&format=json"
 
     # Search the page
     response = requests.get(api + search.format(title=title))
-    # for e in response.json():
-    #     print(e, end="\n\n")
-
     if response.status_code == 200:
+        # for e in response.json():
+        #     print(e, end="\n\n")
         response_json = response.json()
         if (len(response_json[1]) == 0):
             print("Error: No page found")
@@ -22,21 +21,24 @@ def request_wikipedia(title):
 
     # Get the page content
     response = requests.get(api + parse.format(page=page))
-    # for e in response.json():
-    #     print(e, end="\n\n")
-
     if response.status_code == 200:
+        # print(response.content)
+        # for e in response.json():
+        #     print(e, end="\n\n")
         json = response.json()
         if "parse" not in json:
             print("Error: No page found")
             sys.exit(1)
-        content = json["parse"]["text"]["*"]
-        print(dewiki.from_string(content))
-        pass
-        
+        content = json["parse"]["wikitext"]["*"]
+        dewiki_content = dewiki.from_string(content)
     else:
         print("Error: HTTP status code {code}".format(code=response.status_code))
         sys.exit(1)
+
+    # Print the page content in a file named <title>.wiki
+    file = open(page + ".wiki", "w")
+    file.write(dewiki_content)
+    file.close()
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
