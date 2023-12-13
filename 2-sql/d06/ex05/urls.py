7f2578667828fdadd5752d339d3f5f78608fa7ec
Benjamin Perez <bperez@z3r4p3.42lyon.fr>
2023-12-13 20:00:24 +0100

    /home/bperez/Documents/piscine-django/2-sql 05 (not finished)

---
+++ 2-sql/d06/ex05/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from d06.globals import init
+
+from . import views
+
+urlpatterns = [
+    path('populate', views.populate, name='populate'),
+    path('display', views.display, name='display'),
+    path('remove', views.remove, name='remove'),
+]
